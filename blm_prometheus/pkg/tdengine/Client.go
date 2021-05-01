62a1c2b4a6b42c214798a7ed5fc13b300db3e461
zhujinming <zhujinming@accesscorporate.com.cn>
2021-05-01 15:35:23 +0800

    tdengine SQL

--- blm_prometheus/pkg/tdengine/Client.go
+++ blm_prometheus/pkg/tdengine/Client.go
@@ -3,7 +3,6 @@ package tdengine
 import (
 	"blm_prometheus/pkg/log"
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/prompb"
@@ -138,8 +137,7 @@ func (c *Client) buildCommand(q *prompb.Query) (string, error) {
 
 func (c *Client) buildQuery(q *prompb.Query) (string, error) {
 	matchers := make([]string, 0, len(q.Matchers))
-	labelEqualPredicates := make(map[string]string)
-
+	var tableName = ""
 	for _, m := range q.Matchers {
 		escapedName := escapeValue(m.Name)
 		escapedValue := escapeValue(m.Value)
@@ -147,16 +145,14 @@ func (c *Client) buildQuery(q *prompb.Query) (string, error) {
 			switch m.Type {
 			case prompb.LabelMatcher_EQ:
 				if len(escapedValue) == 0 {
-					matchers = append(matchers, fmt.Sprintf("(name IS NULL OR name = '')"))
+					return "", fmt.Errorf("unknown metric name match type %v", m.Type)
 				} else {
-					matchers = append(matchers, fmt.Sprintf("name = '%s'", escapedValue))
+					tableName = escapedValue
 				}
 			case prompb.LabelMatcher_NEQ:
-				matchers = append(matchers, fmt.Sprintf("name != '%s'", escapedValue))
 			case prompb.LabelMatcher_RE:
-				matchers = append(matchers, fmt.Sprintf("name ~ '%s'", anchorValue(escapedValue)))
 			case prompb.LabelMatcher_NRE:
-				matchers = append(matchers, fmt.Sprintf("name !~ '%s'", anchorValue(escapedValue)))
+				return "", fmt.Errorf("no support metric name type %v", m.Type)
 			default:
 				return "", fmt.Errorf("unknown metric name match type %v", m.Type)
 			}
@@ -167,36 +163,29 @@ func (c *Client) buildQuery(q *prompb.Query) (string, error) {
 					// From the PromQL docs: "Label matchers that match
 					// empty label values also select all time series that
 					// do not have the specific label set at all."
-					matchers = append(matchers, fmt.Sprintf("((labels ? '%s') = false OR (labels->>'%s' = ''))",
-						escapedName, escapedName))
+					matchers = append(matchers, fmt.Sprintf("(t_%s = '' or t_%s is null)", escapedName, escapedName))
 				} else {
-					labelEqualPredicates[escapedName] = escapedValue
+					matchers = append(matchers, fmt.Sprintf("t_%s = '%s'", escapedName, escapedValue))
 				}
 			case prompb.LabelMatcher_NEQ:
-				matchers = append(matchers, fmt.Sprintf("labels->>'%s' != '%s'", escapedName, escapedValue))
+				matchers = append(matchers, fmt.Sprintf("t_%s <> '%s'", escapedName, escapedValue))
 			case prompb.LabelMatcher_RE:
-				matchers = append(matchers, fmt.Sprintf("labels->>'%s' ~ '%s'", escapedName, anchorValue(escapedValue)))
+				matchers = append(matchers, fmt.Sprintf("t_%s like '%s'", escapedName, anchorValue(escapedValue)))
 			case prompb.LabelMatcher_NRE:
-				matchers = append(matchers, fmt.Sprintf("labels->>'%s' !~ '%s'", escapedName, anchorValue(escapedValue)))
+				return "", fmt.Errorf("no support match type %v", m.Type)
 			default:
 				return "", fmt.Errorf("unknown match type %v", m.Type)
 			}
 		}
 	}
-	equalsPredicate := ""
-	if len(labelEqualPredicates) > 0 {
-		labelsJSON, err := json.Marshal(labelEqualPredicates)
-
-		if err != nil {
-			return "", err
-		}
-		equalsPredicate = fmt.Sprintf(" AND labels @> '%s'", labelsJSON)
+	if len(tableName) == 0 {
+		return "", fmt.Errorf("unknown tableName")
 	}
 	matchers = append(matchers, fmt.Sprintf("time >= '%v'", toTimestamp(q.StartTimestampMs).Format(time.RFC3339)))
 	matchers = append(matchers, fmt.Sprintf("time <= '%v'", toTimestamp(q.EndTimestampMs).Format(time.RFC3339)))
 
-	return fmt.Sprintf("SELECT time, name, value, labels FROM %s WHERE %s %s ORDER BY time",
-		c.cfg.Table, strings.Join(matchers, " AND "), equalsPredicate), nil
+	return fmt.Sprintf("SELECT time, name, value, labels FROM %s WHERE %s ORDER BY time",
+		tableName, strings.Join(matchers, " AND ")), nil
 }
 
 func escapeValue(str string) string {
@@ -209,18 +198,21 @@ func anchorValue(str string) string {
 	l := len(str)
 
 	if l == 0 || (str[0] == '^' && str[l-1] == '$') {
-		return str
+		str = strings.Replace(str, "$", "", 1)
+		return strings.Replace(str, "^", "", 1)
 	}
 
 	if str[0] == '^' {
-		return fmt.Sprintf("%s$", str)
+		str = strings.Replace(str, "^", "", 1)
+		return fmt.Sprintf("%s%", str)
 	}
 
 	if str[l-1] == '$' {
-		return fmt.Sprintf("^%s", str)
+		str = strings.Replace(str, "$", "", 1)
+		return fmt.Sprintf("%s", "%"+str)
 	}
 
-	return fmt.Sprintf("^%s$", str)
+	return fmt.Sprintf("%s%", "%"+str)
 }
 
 // Name identifies the client as a TDengine client.
